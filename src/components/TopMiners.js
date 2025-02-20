67db8c218aed6a5f84e12f597f889d1169fed7eb
Goblin Saga <167577467+goblinsaga@users.noreply.github.com>
2025-02-19 18:53:55 -0600

    Update TopMiners.js

--- src/components/TopMiners.js
+++ src/components/TopMiners.js
@@ -1,123 +1,66 @@
 import TopStakersContainer from "./TopStakersContainer";
 import RewardPool from "./RewardPool";
-import { Box } from "@chakra-ui/react";
 import ClaimRewardsButton from "./ClaimRewardsButton";
 import RewardPoolFourToTen from "./RewardPool4to10";
 import ClaimRewardsButtonFourToTen from "./ClaimRewardsButton4to10";
-import RewardPoolTopOne from "./RewardPool1";
-import ClaimRewardsButtonTopOne from "./ClaimRewardsTop1";
-import RewardPoolTopTwo from "./RewardPool2";
-import ClaimRewardsButtonTopTwo from "./ClaimRewardsTop2";
 
 const TopMiners = () => {
   return (
     <section id="about">
-      <div id="top" className="container small">
-        <div className="fn_cs_shortabout">
-          <div className="about_left">
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
-              <h3 className="fn__maintitle" data-text="Top Miners">
-                Top Miners
-              </h3>
+      <div id="top" className="container">
+        <h3
+          className="fn__maintitle big"
+          data-text="Top Miners"
+          data-align="center"
+          style={{ marginBottom: "100px" }}
+        >
+          Top Miners
+        </h3>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "10px",
+            paddingBottom: "2rem",
+            flexWrap: "wrap", // Para asegurar que los elementos se ajusten bien en pantalla pequeña.
+          }}
+        >
+          <div
+            style={{
+              width: "260px",
+              flex: "1 1 auto", // Permite que los elementos se adapten.
+            }}
+            className="blog__item"
+          >
+            <div className="meta">
+              <p style={{ textAlign: "center" }}>Top 1 to 3 Rewards</p>
+              <RewardPool />
             </div>
-
-            <div className="meta" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-              <p>Next Claim: 02/15/2025</p>
-            </div>
-
-            <div className="fn_cs_divider">
-              <div className="divider">
-                <span />
-                <span />
-              </div>
-            </div>
-
-            <div>
-              <div className="meta" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <h3 style={{ fontSize: "25px" }} className="fn__maintitle" data-text="Top 1 to 3 Rewards">
-                  Top 1 to 3 Rewards
-                </h3>
-              </div>
-              <div className="title" style={{ paddingTop: "2rem" }}>
-                <RewardPool />
-              </div>
-              <div className="read_more">
-                <Box marginBottom="100px" width="100%" display="flex" justifyContent="center">
-                  <ClaimRewardsButton />
-                </Box>
-              </div>
-            </div>
-
-            <div className="fn_cs_divider">
-              <div className="divider">
-                <span />
-                <span />
-              </div>
-            </div>
-
-            <div>
-              <div className="meta" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <h3 style={{ fontSize: "25px" }} className="fn__maintitle" data-text="Top 3 to 10 Rewards">
-                  Top 3 to 10 Rewards
-                </h3>
-              </div>
-              <div className="title" style={{ paddingTop: "2rem" }}>
-                <RewardPoolFourToTen />
-              </div>
-              <div className="read_more">
-                <Box marginBottom="100px" width="100%" display="flex" justifyContent="center">
-                  <ClaimRewardsButtonFourToTen />
-                </Box>
-              </div>
-            </div>
-
-            <div className="fn_cs_divider">
-              <div className="divider">
-                <span />
-                <span />
-              </div>
-            </div>
-
-            <div>
-              <div className="meta" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <h3 style={{ fontSize: "25px" }} className="fn__maintitle" data-text="Bonus - Top 1 & 2">
-                  Bonus - Top 1 & 2
-                </h3>
-              </div>
-              <div style={{ display: 'flex', gap: '10px', justifyContent: 'center' }}>
-                <div className="title" style={{ paddingTop: "2rem" }}>
-                  <RewardPoolTopOne />
-                </div>
-                <div className="title" style={{ paddingTop: "2rem" }}>
-                  <RewardPoolTopTwo />
-                </div>
-              </div>
-              <div style={{ display: 'flex', gap: '10px', justifyContent: 'center' }}>
-                <div className="read_more">
-                  <Box marginBottom="100px" width="100%" display="flex" justifyContent="center">
-                    <ClaimRewardsButtonTopOne />
-                  </Box>
-                </div>
-                <div className="read_more">
-                  <Box marginBottom="100px" width="100%" display="flex" justifyContent="center">
-                    <ClaimRewardsButtonTopTwo />
-                  </Box>
-                </div>
-              </div>
+            <div style={{ display: 'flex', gap: '10px', justifyContent: 'center' }}>
+              <ClaimRewardsButton />
             </div>
           </div>
 
           <div
-            className="about_right"
             style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              textAlign: 'center'
+              width: "260px",
+              flex: "1 1 auto",
             }}
+            className="blog__item"
           >
-            <TopStakersContainer />
+            <div className="meta">
+              <p style={{ textAlign: "center" }}>Top 4 to 10 Rewards</p>
+              <RewardPoolFourToTen />
+            </div>
+            <div style={{ display: 'flex', gap: '10px', justifyContent: 'center' }}>
+              <ClaimRewardsButtonFourToTen />
+            </div>
           </div>
+        </div>
+        <div>
+          <TopStakersContainer />
         </div>
       </div>
     </section>

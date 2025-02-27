db1d7f5fa4091416ba30a85dd271227034733b92
Goblin Saga <167577467+goblinsaga@users.noreply.github.com>
2025-02-27 12:26:35 -0600

    Update FunFacts.js

--- src/components/FunFacts.js
+++ src/components/FunFacts.js
@@ -18,54 +18,52 @@ const FunFacts = () => {
   return (
     <section id="fun_facts">
       <div className="container">
-        <div className="centered-content">
-          <div className="fn_cs_counter_list">
-            <ul>
-              <li>
-                <div className="item">
-                  <h3 className="fn__gradient_title">
-                    <span className="prefix" />
-                    <Counter end={4444} />
-                    <span className="suffix" />
-                  </h3>
-                  <p>Total Items</p>
-                  <div className="divider" />
-                </div>
-              </li>
-              <li>
-                <div className="item">
-                  <h3 className="fn__gradient_title">
-                    <span className="prefix" />
-                    <GoblinsMinted contractAddress="0x4Ac03107603F37AD24a36c32bEC98b22AF46ABbf" /> {/* Usa el componente aquí */}
-                    <span className="suffix" />
-                  </h3>
-                  <p>Goblins Minted</p>
-                  <div className="divider" />
-                </div>
-              </li>
-              <li>
-                <div className="item">
-                  <h3 className="fn__gradient_title">
-                    <span className="prefix" />
-                    <Counter end={11} />
-                    <span className="suffix" />
-                  </h3>
-                  <p>POL Price</p>
-                  <div className="divider" />
-                </div>
-              </li>
-              <li>
-                <div className="item">
-                  <h3 className="fn__gradient_title">
-                    <span className="prefix" />
-                    <Counter end={4} />
-                    <span className="suffix" />
-                  </h3>
-                  <p>Mint Phase</p>
-                </div>
-              </li>
-            </ul>
-          </div>
+        <div className="fn_cs_counter_list">
+          <ul>
+            <li>
+              <div className="item">
+                <h3 className="fn__gradient_title">
+                  <span className="prefix" />
+                  <Counter end={4444} />
+                  <span className="suffix" />
+                </h3>
+                <p>Total Items</p>
+                <div className="divider" />
+              </div>
+            </li>
+            <li>
+              <div className="item">
+                <h3 className="fn__gradient_title">
+                  <span className="prefix" />
+                  <GoblinsMinted contractAddress="0x4Ac03107603F37AD24a36c32bEC98b22AF46ABbf" /> {/* Usa el componente aquí */}
+                  <span className="suffix" />
+                </h3>
+                <p>Goblins Minted</p>
+                <div className="divider" />
+              </div>
+            </li>
+            <li>
+              <div className="item">
+                <h3 className="fn__gradient_title">
+                  <span className="prefix" />
+                  <Counter end={formattedTokensClaimed} decimals={0} /> {/* Utiliza el valor formateado */}
+                  <span className="suffix">M</span>
+                </h3>
+                <p>Tokens Mined</p>
+                <div className="divider" />
+              </div>
+            </li>
+            <li>
+              <div className="item">
+                <h3 className="fn__gradient_title">
+                  <span className="prefix" />
+                  <Counter end={11} />
+                  <span className="suffix">POL</span>
+                </h3>
+                <p>Mint Price</p>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </section>

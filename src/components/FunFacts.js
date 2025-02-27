de358e25216ae1a7ee890a23b01d1b7dad155029
Goblin Saga <167577467+goblinsaga@users.noreply.github.com>
2025-02-26 22:57:23 -0600

    Update FunFacts.js

--- src/components/FunFacts.js
+++ src/components/FunFacts.js
@@ -18,31 +18,32 @@ const FunFacts = () => {
   return (
     <section id="fun_facts">
       <div className="container">
-        <div className="fn_cs_counter_list">
-          <ul>
-            <li>
-              <div className="item">
-                <h3 className="fn__gradient_title">
-                  <span className="prefix" />
-                  <Counter end={4444} />
-                  <span className="suffix" />
-                </h3>
-                <p>Total Items</p>
-                <div className="divider" />
-              </div>
-            </li>
-            <li>
-              <div className="item">
-                <h3 className="fn__gradient_title">
-                  <span className="prefix" />
-                  <GoblinsMinted contractAddress="0x4Ac03107603F37AD24a36c32bEC98b22AF46ABbf" /> {/* Usa el componente aquí */}
-                  <span className="suffix" />
-                </h3>
-                <p>Goblins Minted</p>
-                <div className="divider" />
-              </div>
-            </li>
-          </ul>
+        <div className="centered-content" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <div className="fn_cs_counter_list">
+            <ul>
+              <li>
+                <div className="item">
+                  <h3 className="fn__gradient_title">
+                    <span className="prefix" />
+                    <Counter end={4444} />
+                    <span className="suffix" />
+                  </h3>
+                  <p>Total Items</p>
+                  <div className="divider" />
+                </div>
+              </li>
+              <li>
+                <div className="item">
+                  <h3 className="fn__gradient_title">
+                    <span className="prefix" />
+                    <GoblinsMinted contractAddress="0x4Ac03107603F37AD24a36c32bEC98b22AF46ABbf" /> {/* Usa el componente aquí */}
+                    <span className="suffix" />
+                  </h3>
+                  <p>Goblins Minted</p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </section>

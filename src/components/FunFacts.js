7ffe54cd5ef738e65d047e3fea1293eb0e339569
Goblin Saga <167577467+goblinsaga@users.noreply.github.com>
2025-02-26 23:10:17 -0600

    Update FunFacts.js

--- src/components/FunFacts.js
+++ src/components/FunFacts.js
@@ -40,6 +40,28 @@ const FunFacts = () => {
                     <span className="suffix" />
                   </h3>
                   <p>Goblins Minted</p>
+                  <div className="divider" />
+                </div>
+              </li>
+              <li>
+                <div className="item">
+                  <h3 className="fn__gradient_title">
+                    <span className="prefix" />
+                    <Counter end={11} />
+                    <span className="suffix" />
+                  </h3>
+                  <p>POL Price</p>
+                  <div className="divider" />
+                </div>
+              </li>
+              <li>
+                <div className="item">
+                  <h3 className="fn__gradient_title">
+                    <span className="prefix" />
+                    <Counter end={4} />
+                    <span className="suffix" />
+                  </h3>
+                  <p>Mint Phase</p>
                 </div>
               </li>
             </ul>

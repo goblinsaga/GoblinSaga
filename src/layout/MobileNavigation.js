f65c851010e429857e19fdd702371525f4ea4ff1
Goblin Saga <167577467+goblinsaga@users.noreply.github.com>
2025-03-04 22:44:41 -0600

    Update MobileNavigation.js

--- src/layout/MobileNavigation.js
+++ src/layout/MobileNavigation.js
@@ -25,7 +25,7 @@ const MobileNavigation = ({ walletToggle, navigationToggle }) => {
           </div>
           <div className="wallet">
             <a
-              href="https://app.goblinsaga.club"
+              href="https://app.goblinsaga.xyz"
               className="metaportal_fn_button wallet_opener"
             >
               <span>Launch App</span>

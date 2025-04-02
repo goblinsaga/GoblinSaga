546bb97c6ef885e652ad6a4f08b1c2a9395449f7
Goblin Saga <167577467+goblinsaga@users.noreply.github.com>
2025-04-02 14:26:17 -0600

    Update mint.js

--- pages/mint.js
+++ pages/mint.js
@@ -18,7 +18,7 @@ const Nft = ({ getSingleNft, nft, getNfts, nfts }) => {
   const { id } = router.query;
   const [similarItem, setSimilarItem] = useState([]);
   const [quantity, setQuantity] = useState(1); // Cantidad inicial
-  const pricePerNft = 11; // Precio por NFT
+  const pricePerNft = 5; // Precio por NFT
   const [errorMessage, setErrorMessage] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
   const address = useAddress();
@@ -155,7 +155,7 @@ const Nft = ({ getSingleNft, nft, getNfts, nfts }) => {
           <div className="metaportal_fn_mintbox">
             <div className="mint_left">
               <div className="mint_title">
-                <span>Mint Phase 4 - 27% OFF</span>
+                <span>Mint Phase 4/5</span>
               </div>
               <div className="mint_list">
                 <ul>
@@ -163,7 +163,7 @@ const Nft = ({ getSingleNft, nft, getNfts, nfts }) => {
                     <div className="item">
                       <h4>NFT Price</h4>
                       <div>
-                        <h3>{pricePerNft} <img src="/img/POL.png" style={{ width: "25px", marginTop: "-5px" }} /> <span style={{ fontSize: "11px" }}>(Polygon)</span></h3>
+                        <h3>{pricePerNft} <img src="/img/USDT.png" style={{ width: "25px", marginTop: "-5px" }} /> <span style={{ fontSize: "11px" }}>(Polygon)</span></h3>
                       </div>
                     </div>
                   </li>
@@ -188,7 +188,7 @@ const Nft = ({ getSingleNft, nft, getNfts, nfts }) => {
                       <h4>Total Price</h4>
                       <h3>
                         <span className="total_price">
-                          {totalPrice} <img src="/img/POL.png" style={{ width: "25px", marginTop: "-5px" }} /> <span style={{ fontSize: "11px" }}>(+ GAS)</span>
+                          {totalPrice} <img src="/img/USDT.png" style={{ width: "25px", marginTop: "-5px" }} /> <span style={{ fontSize: "11px" }}>(+ GAS)</span>
                         </span>
                       </h3>
                     </div>
@@ -262,7 +262,7 @@ const Nft = ({ getSingleNft, nft, getNfts, nfts }) => {
                           error.message.includes("missing revert data")
                         ) {
                           setErrorMessage(
-                            `Minting failed: Insufficient funds. Mint price 11.0 POL.`
+                            `Minting failed: Insufficient funds. Mint price 5.0 USDT.`
                           );
                         } else {
                           setErrorMessage(`Minting failed: ${errorName}`);
@@ -360,7 +360,7 @@ const Nft = ({ getSingleNft, nft, getNfts, nfts }) => {
                     <span className="text">Phase 4:</span>
                   </p>
                   <div style={{ marginBottom: "10px" }}>
-                    <span className="status"><p style={{ color: "#00fc00" }}>Live: 15 <img src="/img/POL.png" style={{ width: "20px", marginTop: "3px", marginLeft: "3px" }} /></p></span>
+                    <span className="status"><p style={{ color: "#00fc00" }}>Live: 5 <img src="/img/USDT.png" style={{ width: "20px", marginTop: "3px", marginLeft: "3px" }} /></p></span>
                   </div>
                   <p>
                     <span className="text">Phase 5:</span>

3b0a38c8b69bc7a87817f95459dcb94e42fc76fb
Goblin Saga <167577467+goblinsaga@users.noreply.github.com>
2025-04-06 14:24:18 -0600

    Update mint.js

--- pages/mint.js
+++ pages/mint.js
@@ -18,7 +18,7 @@ const Nft = ({ getSingleNft, nft, getNfts, nfts }) => {
   const { id } = router.query;
   const [similarItem, setSimilarItem] = useState([]);
   const [quantity, setQuantity] = useState(1); // Cantidad inicial
-  const pricePerNft = 5.0; // Precio por NFT
+  const pricePerNft = 4; // Precio por NFT
   const [errorMessage, setErrorMessage] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
   const address = useAddress();
@@ -163,7 +163,7 @@ const Nft = ({ getSingleNft, nft, getNfts, nfts }) => {
                     <div className="item">
                       <h4>NFT Price</h4>
                       <div>
-                        <h3>5.0 <img src="/img/USDT.png" style={{ width: "20px", marginTop: "-5px" }} /> <span style={{ fontSize: "11px" }}>(Polygon)</span></h3>
+                        <h3>4.0 <img src="/img/USDT.png" style={{ width: "20px", marginTop: "-5px" }} /> <span style={{ fontSize: "11px" }}>(Polygon)</span></h3>
                       </div>
                     </div>
                   </li>
@@ -262,7 +262,7 @@ const Nft = ({ getSingleNft, nft, getNfts, nfts }) => {
                           error.message.includes("missing revert data")
                         ) {
                           setErrorMessage(
-                            `Minting failed: Insufficient funds. Mint price 5.0 USDT.`
+                            `Minting failed: Insufficient funds. Mint price 4.0 USDT.`
                           );
                         } else {
                           setErrorMessage(`Minting failed: ${errorName}`);
@@ -360,7 +360,7 @@ const Nft = ({ getSingleNft, nft, getNfts, nfts }) => {
                     <span className="text">Phase 4:</span>
                   </p>
                   <div style={{ marginBottom: "10px" }}>
-                    <span className="status"><p style={{ color: "#00fc00" }}>Live: 5 <img src="/img/USDT.png" style={{ width: "18px", marginTop: "-3px", marginLeft: "3px" }} /></p></span>
+                    <span className="status"><p style={{ color: "#00fc00" }}>Live: 4 <img src="/img/USDT.png" style={{ width: "18px", marginTop: "-3px", marginLeft: "3px" }} /></p></span>
                   </div>
                   <p>
                     <span className="text">Phase 5:</span>
